fd152326ec650b7d6835e5152565226e2f791eb2
Rauf Abbas <rafo.abbas@gmail.coma>
2023-05-09 22:41:06 +0400

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>@yield('title', 'Dashboard') - Gönderim sistemi</title>
+    <title>@yield('title', 'Dashboard') - Admin</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Rauf Abbas" name="author">
     <!-- CSRF Token -->
